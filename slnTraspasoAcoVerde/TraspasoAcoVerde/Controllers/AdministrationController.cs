12e73df23a6abdeb2c6c1bbcd4a21bc6d522840a
Adan Arik Villamar Campos <villamar.sys@gmail.com>
2018-12-11 17:48:05 -0600

    Commit Initial Base
    Commit Inicial Base para proyecto Acosta Verde

---
+++ slnTraspasoAcoVerde/TraspasoAcoVerde/Controllers/AdministrationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TraspasoAcoVerde.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Configuration
+        public ActionResult Configuration()
+        {
+            return View();
+        }
+
+        public ActionResult Roles()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+    }
+}
---
+++ slnTraspasoAcoVerde/TraspasoAcoVerde/Controllers/PlazaController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace TraspasoAcoVerde.Controllers
+{
+    public class PlazaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Technicals()
+        {
+            return View();
+        }
+    }
+}
