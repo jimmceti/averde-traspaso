0a21d236e15a98fb46241a6ce52e66417c8b3e37
jignaciomm <jimmceti.dev@outlook.com>
2019-01-05 17:55:00 -0600

    Se agregan pendientes de clases, metodos y variables de prueba exitosa de consulta de procedimientos de bd por medio de la DLL

---
+++ slnTraspasoAcoVerde/Acosta.Data/ProceduresName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acosta.Data
+{
+    public static class ProceduresName
+    {
+        /// <summary>
+        /// Procedimiento sel_FechaHoraSQL de prueba de consumo de la base de datos BD_AVFT
+        /// </summary>
+        public const string sel_FechaHoraSQL = "sel_FechaHoraSQL";
+
+    }
+}
