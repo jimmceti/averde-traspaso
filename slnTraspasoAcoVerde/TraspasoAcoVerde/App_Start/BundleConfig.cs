ae4c84676f04587a858df14218eafd187093981e
jignaciomm <jimmceti.dev@outlook.com>
2019-01-06 17:28:39 -0600

    Se organizan archivos del plugin JSGRID y se implementa exitosamente funcionalidad de mostrar registros en el grid Administracion de Usuarios

--- slnTraspasoAcoVerde/TraspasoAcoVerde/App_Start/BundleConfig.cs
+++ slnTraspasoAcoVerde/TraspasoAcoVerde/App_Start/BundleConfig.cs
@@ -18,19 +18,26 @@ namespace TraspasoAcoVerde
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
-            
+
+
+            /*
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            "~/Scripts/jquery-3.3.1.min.js"));
+            */
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/Scripts/jsAppProject").Include(
-                    "~/Scripts/jsgrid.js",
+                    "~/Scripts/jsgrid_1_5_3/jsgrid_1_5_3.min.js",
                     "~/Scripts/Project/Project.js",
                     "~/Scripts/Project/Admon/Users.js"
                 ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/Bootstrap-4/bootstrap.css",
-                      "~/Content/css/custom.css"                
+                      "~/Content/css/custom.css",
+                      "~/Scripts/jsgrid_1_5_3/jsgrid_1_5_3.min.css",
+                      "~/Scripts/jsgrid_1_5_3/jsgrid_1_5_3_theme.min.css"
             ));
 
         }
--- slnTraspasoAcoVerde/TraspasoAcoVerde/Scripts/Project/Admon/Users.js
+++ slnTraspasoAcoVerde/TraspasoAcoVerde/Scripts/Project/Admon/Users.js
@@ -9,18 +9,20 @@ jsproosis.Project.AdmonUsers = (function ($, window, document, navigator, localS
     };
 
     var loadUsers = function () {
+        
         $.ajax(
             {
                 type: 'POST',
                 url: URLApp + 'Administration/LoadUsers',
                 dataType: "json"
-            }).done(function (dataUsers) {
-                FillGridUsers(dataUsers);
+            }).done(function (SearchUsers) {
+                FillGridUsers(SearchUsers);
             });
+        
     };
 
-
-    var FillGridUsers = function (data) {
+    
+    var FillGridUsers = function (Valores) {
         $("#grdUsers").jsGrid('destroy');
         console.log('Aqui despues de destruid el grid');
         $("#grdUsers").jsGrid({
@@ -44,33 +46,32 @@ jsproosis.Project.AdmonUsers = (function ($, window, document, navigator, localS
             pageLastText: "Ultima",
             loadMessage: "Por favor, espera...",
             updateOnResize: true,
-            //autowith: true,
+            autowith: true,
             shrinkToFit: false,
             controller: {
-                loadData: function (parametros) {
-                    if (data != null) {
-                        var gridLoadData = data;
-                        return $.grep(gridLoadData, function (parametro) {
-                            return (!parametro.NombreCompleto || parseInt(parametro.NombreCompleto) == parametros.NombreCompleto);
-                        });
-                    }
+                loadData: function (filter) {
+
+                    var gridLoadData = Valores.data;
+
+                    return $.grep(gridLoadData, function (user) {
+                        return (!filter.IdUsuario || user.IdUsuario.indexOf(filter.IdUsuario) > -1) &&
+                            (!filter.NombreCompleto || user.NombreCompleto === filter.NombreCompleto);
+                    });
                 }
             },
             fields: [
-                { name: "IdUsuario", type: "number", title: "ID USUARIO", width: 200, editing: false, align: "center", filtering: false, fixed: true },
+                { name: "IdUsuario", type: "number", title: "ID USUARIO", width: 200, editing: false, align: "center", filtering: true, fixed: true },
                 { name: "NombreCompleto", type: "text", title: "NOMBRE COMPLETO", width: 550, editing: false, align: "center", filtering: false, fixed: true },
                 { name: "Correo", type: "text", title: "CORREO", width: 350, editing: false, align: "center", filtering: false, fixed: false },
                 { name: "TipoUsuario", type: "text", title: "TIPO USUARIO", width: 200, editing: false, align: "center", filtering: false, fixed: true },
-                { name: "Activo", type: "text", title: "ACTIVO", width: 100, editing: false, align: "center", filtering: false, fixed: true }
+                { name: "Activo", type:"checkbox", title: "ACTIVO", width: 100, editing: false, align: "center", filtering: false, fixed: true }
             ]
             //,rowClick: function (args) {
             //    InicializarGridDocumentos(args.item.Pago_DocumentosRelacionado);
             //
             //}
         });
-
-    };
-
+    };    
 
 
     return {
